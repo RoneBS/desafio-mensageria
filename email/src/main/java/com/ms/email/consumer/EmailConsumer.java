a13401fa704bee2d29a61191949a0d73a96aacf3
RoneBS <ronebs10@hotmail.com>
2025-08-26 13:17:05 -0300

    EmailService iniciado, EmailConsumer adicionado BeanUtils para converter dto para entidade

--- email/src/main/java/com/ms/email/consumer/EmailConsumer.java
+++ email/src/main/java/com/ms/email/consumer/EmailConsumer.java
@@ -1,7 +1,9 @@
 package com.ms.email.consumer;
 
 import com.ms.email.api.dto.EmailRecordDto;
+import com.ms.email.domain.entities.Email;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +12,7 @@ public class EmailConsumer {
 
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenerEmailQueue(@Payload EmailRecordDto emailRecordDto) {
-        System.out.println(emailRecordDto.emailTo());
+        var email = new Email();
+        BeanUtils.copyProperties(emailRecordDto, email);
     }
 }
---
+++ email/src/main/java/com/ms/email/domain/service/UserService.java
@@ -0,0 +1,7 @@
+package com.ms.email.domain.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
